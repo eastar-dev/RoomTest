556cf6d7bf60bef08303ab0fbaa0cb78418ef3e1
eastar <eastarj@gmail.com>
2021-10-02 23:07:12 +0900

    set click event

--- app/src/main/java/dev/eastar/roomtest/MainActivity.kt
+++ app/src/main/java/dev/eastar/roomtest/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.eastar.roomtest
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -8,12 +9,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.eastar.roomtest.ui.theme.RoomTestTheme
@@ -35,15 +37,22 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainApp() {
+    val context = LocalContext.current
     LazyColumn(
         modifier = Modifier.fillMaxWidth(),
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
         verticalArrangement = Arrangement.spacedBy(4.dp),
     ) {
+
         item {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = "First item")
+            TextButton(onClick = {
+                Toast.makeText(context, "You clicked First item", Toast.LENGTH_SHORT).show()
+            }) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "First item"
+                )
+            }
         }
 
         // Add 5 items
