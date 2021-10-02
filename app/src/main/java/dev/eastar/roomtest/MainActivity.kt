39bd254541f27fb077467ea7836febbf91c7b86e
eastar <eastarj@gmail.com>
2021-10-02 23:40:43 +0900

    gradle update

--- app/src/main/java/dev/eastar/roomtest/MainActivity.kt
+++ app/src/main/java/dev/eastar/roomtest/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -24,20 +26,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val context = LocalContext.current
             RoomTestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MainApp()
+                    MainApp() {
+                        Toast.makeText(context, "You clicked First item", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun MainApp() {
-    val context = LocalContext.current
+fun MainApp(onItemClicked: (String) -> Unit = {}) {
     LazyColumn(
         modifier = Modifier.fillMaxWidth(),
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
@@ -45,19 +49,21 @@ fun MainApp() {
     ) {
 
         item {
-            TextButton(onClick = {
-                Toast.makeText(context, "You clicked First item", Toast.LENGTH_SHORT).show()
+            ListItem(Modifier.clickable {
+                onItemClicked("First item")
             }) {
                 Text(
                     modifier = Modifier.fillMaxWidth(),
                     text = "First item"
                 )
             }
+            Divider()
         }
 
         // Add 5 items
-        items(100) { index ->
+        items(50) { index ->
             Text(text = "Item: $index")
+            Divider()
         }
 
         // Add another single item
