f71ac6074b25b324053fa58734b41dcd4326eedf
eastar <eastarj@gmail.com>
2021-10-08 02:22:44 +0900

    room automigration 1 2 column add

--- data/src/main/java/dev/eastar/roomtest/data/db/TestDatabase.kt
+++ data/src/main/java/dev/eastar/roomtest/data/db/TestDatabase.kt
@@ -1,10 +1,16 @@
 package dev.eastar.roomtest.data.db
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [UserEntity::class], version = 1, exportSchema = true)
+@Database(
+    entities = [UserEntity::class],
+    autoMigrations = [AutoMigration(from = 1, to = 2)],
+    version = 2,
+    exportSchema = true
+)
 @TypeConverters(DateConverter::class)
 abstract class TestDatabase : RoomDatabase() {
     abstract fun getUserDao(): UserDao
